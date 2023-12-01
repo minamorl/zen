fad10f9886fcc519ae3b15c9a564974c14a6f5f2
minamorl <minamorl@users.noreply.github.com>
2023-12-01 16:12:15 +0900

    wip

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,14 +1,6 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import { trpc } from '../utils/trpc';
-import { useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-
-type Inputs = {
-  email: string
-  password: string
-};
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwaXd4bXRrbXltcWx0ZGZ4b2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyNjk1NzMsImV4cCI6MjAxNjg0NTU3M30.aYkKWGpKvfoOj84p5T4lED0Iy6VXK8yH3Jo0h2kEQMc'
 const supabase = createClient('https://spiwxmtkmymqltdfxogg.supabase.co', SUPABASE_ANON_KEY)
@@ -23,6 +15,4 @@ export default function SignInPage() {
     redirectTo="/"
   />
   );
-
-  
 }
