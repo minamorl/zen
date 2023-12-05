3a68736728b7004e7ce43ba0c1da7631d395b210
minamorl <minamorl@users.noreply.github.com>
2023-12-05 16:01:35 +0900

    wip

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -4,6 +4,7 @@ import { CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { PrismaClient } from "@prisma/client";
 import { lucia } from "lucia";
 import { prisma } from "@lucia-auth/adapter-prisma";
+import { web } from "lucia/middleware";
 
 const prismaClient = new PrismaClient();
 
@@ -14,13 +15,23 @@ const auth = lucia({
     session: "session",
   }),
   env: "DEV",
+  middleware: web(),
+  sessionCookie: {
+    expires: false,
+  },
 });
 
 export const createContext = async ({ req, res }: CreateNextContextOptions) => {
+  const sessionId = auth.readSessionCookie(req.headers.cookie);
+  let session = null;
+  if (sessionId) session = await auth.getSession(sessionId);
+  console.log(session);
+
   // Use the singleton Prisma client and Lucia auth in the context
   return {
     prisma: prismaClient,
     auth,
+    session,
     req,
     res,
   };
