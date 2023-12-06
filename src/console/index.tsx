3ecf45a5b2901a5aa55e5fbc5b280defab1c1ebe
minamorl <minamorl@users.noreply.github.com>
2023-12-06 12:55:29 +0900

    Hi I'm Navi

--- src/console/index.tsx
+++ src/console/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useConsole } from "../context/consoleContext";
 type ConsoleProps = {
   message: string;
@@ -6,12 +6,19 @@ type ConsoleProps = {
 
 export const ConsoleUI: React.FC<ConsoleProps> = () => {
   const [message] = useConsole();
+  const [mouseOver, setMouseOver] = useState(false);
 
   return (
-    <div className="text-sm font-mono">
+    <div
+      className="text-sm font-mono cursor-default"
+      onMouseOver={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
+    >
       <div className="flex flex-row">
         <div className="block w-6">⚡</div>
-        <div className="block">{message}</div>
+        <div className="block">
+          {mouseOver ? "Hi! I'm Navi. What are you looking for?" : message}
+        </div>
         <div className="block animate-ping">_</div>
       </div>
     </div>
