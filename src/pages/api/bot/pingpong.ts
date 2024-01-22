c74674acd3fedb55ad776e4beaaca34e99e0cc35
minamorl <minamorl@users.noreply.github.com>
2024-01-22 17:02:11 +0900

    Impl simple Ping Pong bot

---
+++ src/pages/api/bot/pingpong.ts
@@ -0,0 +1,42 @@
+import { createContext } from "@/server/trpc";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  status: string;
+};
+type ParsedData = {
+  event: string;
+  value: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
+  if (req.method === "POST") {
+    const parsed = parse(req.body);
+    const ctx = await createContext({ req, res });
+    if (parsed.value.post.content.startsWith("/ping")) {
+      const post = await ctx.prisma.post.create({
+        data: {
+          persona: {
+            connect: {
+              id: parsed.value.post.personaId,
+            },
+          },
+          content: "=============== PONG ===============",
+          board: {
+            connect: {
+              title: "test",
+            },
+          },
+        },
+      });
+    }
+  }
+  res.status(200).json({ status: "ok" });
+}
+
+function parse(str: string): ParsedData {
+  return JSON.parse(str);
+}
