f254cc888e898334e4d0dd599591ba58a8010bf8
minamorl <minamorl@users.noreply.github.com>
2024-03-23 11:52:33 +0900

    wip

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    S3_UPLOAD_KEY: process.env.S3_UPLOAD_KEY,
+    S3_UPLOAD_SECRET: process.env.S3_UPLOAD_SECRET,
+    S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
+    S3_UPLOAD_ENDPOINT: process.env.S3_UPLOAD_ENDPOINT,
+    S3_UPLOAD_PORT: process.env.S3_UPLOAD_PORT,
+    S3_UPLOAD_REGION: process.env.S3_UPLOAD_REGION,
+  },
+};
 
 module.exports = nextConfig;
